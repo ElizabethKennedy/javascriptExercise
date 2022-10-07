cf0bbca744ddcf5e4a40d592776aa1ba613d98f6
Elizabeth Kennedy <eakennedy.2412@gmail.com>
2022-10-07 10:53:59 -0700

    added fleur-de-lis background

---
+++ app.js
@@ -0,0 +1,43 @@
+const firstName = "Elizabeth";
+let lastName;
+var age;
+
+lastName = "Kennedy";
+age = 38;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+
+
+let language = "Javascript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(language);
+console.log(createdYear);
+console.log(isCaseSensitive);
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let selectedSize = "M";
+
+console.log(price);
+console.log(isOnSale);
+console.log(salePercentage);
+console.log(stock);
+console.log(selectedSize);
+
+let title = "Lolita";
+let author = "Vladimir Nabokov";
+let pageCount = 336;
+let bookmark = 336;
+let hasRead = true;
+
+console.log(title);
+console.log(author);
+console.log(pageCount);
+console.log(bookmark);
+console.log(hasRead);
